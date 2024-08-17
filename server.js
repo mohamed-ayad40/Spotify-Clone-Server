2744a547183ee5c06fc090cc20c710ec3a1d3a13
mohamed-ayad40 <mohamedayad5600@gmail.com>
2024-08-18 01:52:46 +0300

    s

--- server.js
+++ server.js
@@ -33,7 +33,7 @@ const port = process.env.PORT || 4000;
 // app.use(cors(corsOptions));
 app.use(cors({
     // origin: process.env.CLIENT_HOSTED_URL,
-    origin: "https://mohamed-ayad40.github.io/Spotify-Clone-3",
+    origin: "https://mohamed-ayad40.github.io",
     // origin: "http://localhost:5173",
     credentials: true,
     methods: "GET,POST,PUT,DELETE",
