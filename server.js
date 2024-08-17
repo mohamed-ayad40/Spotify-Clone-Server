166d659f4524e46fcd94b1535d3046061402bc0a
mohamed-ayad40 <mohamedayad5600@gmail.com>
2024-08-17 23:28:43 +0300

    س

--- server.js
+++ server.js
@@ -33,6 +33,7 @@ const port = process.env.PORT || 4000;
 // app.use(cors(corsOptions));
 app.use(cors({
     origin: process.env.CLIENT_HOSTED_URL,
+    origin: "https://mohamed-ayad40.github.io",
     // origin: "http://localhost:5173",
     credentials: true,
     methods: "GET,POST,PUT,DELETE",
