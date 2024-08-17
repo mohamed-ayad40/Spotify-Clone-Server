544f9c690f3260394643c2db8c6e3b0c15695de5
mohamed-ayad40 <mohamedayad5600@gmail.com>
2024-08-17 23:31:07 +0300

    s

--- server.js
+++ server.js
@@ -32,7 +32,7 @@ const port = process.env.PORT || 4000;
 // }));
 // app.use(cors(corsOptions));
 app.use(cors({
-    origin: process.env.CLIENT_HOSTED_URL,
+    // origin: process.env.CLIENT_HOSTED_URL,
     origin: "https://mohamed-ayad40.github.io",
     // origin: "http://localhost:5173",
     credentials: true,
