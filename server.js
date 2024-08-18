842ff42a4319ea19cda71b764dd16d3505c14430
mohamed-ayad40 <mohamedayad5600@gmail.com>
2024-08-18 20:35:28 +0300

    s

--- server.js
+++ server.js
@@ -34,8 +34,8 @@ const port = process.env.PORT || 4000;
 
 app.use(cors({
     // origin: process.env.CLIENT_HOSTED_URL,
-    origin: "https://spotify-clone-3-psi.vercel.app",
-    // origin: "http://localhost:5173",
+    // origin: "https://spotify-clone-3-psi.vercel.app",
+    origin: "http://localhost:5173",
     credentials: true,
     methods: "GET,POST,PUT,DELETE",
     sameSite: "none"
